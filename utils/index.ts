83418b60dc952c1b04dd4ab6d0eb4665791652e1
Tomoya148665 <kopeyama27@gmail.com>
2023-12-23 16:47:49 +0900

    ss

---
+++ utils/index.ts
@@ -0,0 +1,34 @@
+import { CarProps } from "@/types";
+
+export async function fetchCars() {
+  const headers = {
+    "X-RapidAPI-Key": "8772b54ebcmsh11dc00013f38572p10345djsnb387e7ae9a99",
+    "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
+  };
+
+  const response = await fetch(
+    "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla",
+    {
+      headers: headers,
+    }
+  );
+
+  const result = await response.json();
+
+  return result;
+}
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+  const url = new URL("https://cdn.imagin.studio/getimage");
+
+  const { make, year, model } = car;
+
+  url.searchParams.append("customer", "hrjavascript-mastery");
+  url.searchParams.append("make", make);
+  url.searchParams.append("modelFamily", model.split(" ")[0]);
+  url.searchParams.append("zoomType", "fullscreen");
+  url.searchParams.append("modelYear", `${year}`);
+  url.searchParams.append("angle", `${angle}`);
+
+  return `$(url)`;
+};
